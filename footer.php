03c6b5f469c6e313317afc3b6b04cc10bc55d85f
Brian Ressler <carlofabyss@hotmail.com>
2024-03-11 01:39:52 -0500

    Start adding jobpost single template and make some fixes to mobile navigation due to necessary updates

--- footer.php
+++ footer.php
@@ -20,7 +20,59 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 }
 ?>
 
-<?php wp_footer(); ?>
+<?php 
+wp_footer(); 
+
+$header_nav_menu_mobile = wp_nav_menu( [
+	'theme_location' => 'menu-1',
+	'fallback_cb' => false,
+	'echo' => false,
+	'menu_class' => 'eccent_mobile_menu',
+] );
+
+$logo_output = '';
+
+if( function_exists( 'the_custom_logo' ) ) {
+
+    if(has_custom_logo()) {
+		$logo = get_theme_mod( 'custom_logo' );
+		$logo_image = wp_get_attachment_image_src( $logo , 'medium' );
+
+        $logo_output = '<img src="' . $logo_image[0] . '">';
+    } else {
+        $logo_output = '<img src="' . get_stylesheet_directory_uri() . '/img/cdcn_logo_current_white.png">';
+    }
+}
+
+$cdcn_setting_headerphone = get_theme_mod('cdcn_setting_headerphone', '(000) 000-0000');
+$cdcn_setting_headerfacebook = get_theme_mod('cdcn_setting_headerfacebook', 'https://facebook.com');
+$cdcn_setting_headerlinkedin = get_theme_mod('cdcn_setting_headerlinkedin', 'https://linkedin.com');
+$cdcn_setting_headeryoutube = get_theme_mod('cdcn_setting_headeryoutube', 'https://youtube.com');
+
+$header_phone_output = '';
+$header_facebook_output = '';
+$header_linkedin_output = '';
+$header_youtube_output = '';
+
+if($cdcn_setting_headerphone != "" && empty($cdcn_setting_headerphone) == false) {
+	$phone_unformatted = preg_replace('/\D+/', '', $cdcn_setting_headerphone);
+	$header_phone_output = '<a class="eccent_header_phone" href="tel:' . $phone_unformatted . '"><img src="' . get_stylesheet_directory_uri() . '/svg/viber.svg"><span>' . $cdcn_setting_headerphone . '</span></a>';
+} else {
+	$header_phone_output = '<a class="eccent_header_phone" href="tel:0000000000"><img src="' . get_stylesheet_directory_uri() . '/svg/viber.svg"><span>(000) 000-0000</span></a>';
+}
+
+if($cdcn_setting_headerfacebook != "" && empty($cdcn_setting_headerfacebook) == false) {
+	$header_facebook_output = '<a href="' . $cdcn_setting_headerfacebook . '" target="_blank"><img src="' . get_stylesheet_directory_uri() . '/svg/social/footericonone.svg"></a>';
+}
+
+if($cdcn_setting_headerlinkedin != "" && empty($cdcn_setting_headerlinkedin) == false) {
+	$header_linkedin_output = '<a href="' . $cdcn_setting_headerlinkedin . '" target="_blank"><img src="' . get_stylesheet_directory_uri() . '/svg/social/footericontwo.svg"></a>';
+}
+
+if($cdcn_setting_headeryoutube != "" && empty($cdcn_setting_headeryoutube) == false) {
+	$header_youtube_output = '<a href="' . $cdcn_setting_headeryoutube . '" target="_blank"><img src="' . get_stylesheet_directory_uri() . '/svg/social/footericonthree.svg"></a>';
+}
+?>
 
 <div id="cdcn_vlightbox" class="cdcn_vlightbox">
 	<div id="cdcn_vlb_overlay" class="cdcn_vlb_overlay"></div>
@@ -37,5 +89,16 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 		</div>
 	</div>
 </div>
+
+<?php
+if ( $header_nav_menu_mobile ) {
+	echo '<div id="eccent_mobile_wrapper" class="eccent_mobile_wrapper">
+		<div id="eccent_mobile_controls" class="eccent_mobile_controls">' . $logo_output . '<button id="eccent_mobile_close" class="eccent_mobile_close"><img src="' . get_stylesheet_directory_uri() . '/svg/close.svg"></button></div>' 
+		. $header_nav_menu_mobile . 
+	'<div id="eccent_mobile_social" class="eccent_mobile_social">' . 
+		$header_facebook_output . $header_linkedin_output . $header_youtube_output . 
+	'</div></div>';
+}
+?>
 </body>
 </html>
--- template-parts/header.php
+++ template-parts/header.php
@@ -17,13 +17,6 @@ $header_nav_menu = wp_nav_menu( [
 	'menu_class' => 'eccent_desktop_menu',
 ] );
 
-$header_nav_menu_mobile = wp_nav_menu( [
-	'theme_location' => 'menu-1',
-	'fallback_cb' => false,
-	'echo' => false,
-	'menu_class' => 'eccent_mobile_menu',
-] );
-
 $logo_output = '';
 
 if( function_exists( 'the_custom_logo' ) ) {
@@ -79,15 +72,6 @@ if( function_exists( 'the_custom_logo' ) ) {
 
 					if ( $header_nav_menu ) {
 						echo $header_nav_menu;
-					}
-
-					if ( $header_nav_menu_mobile ) {
-						echo '<div id="eccent_mobile_wrapper" class="eccent_mobile_wrapper">
-							<div id="eccent_mobile_controls" class="eccent_mobile_controls">' . $logo_output . '<button id="eccent_mobile_close" class="eccent_mobile_close"><img src="' . get_stylesheet_directory_uri() . '/svg/close.svg"></button></div>' 
-							. $header_nav_menu_mobile . 
-						'<div id="eccent_mobile_social" class="eccent_mobile_social">' . 
-							$header_facebook_output . $header_linkedin_output . $header_youtube_output . 
-						'</div></div>';
 					}
 
 					echo $header_phone_output;
